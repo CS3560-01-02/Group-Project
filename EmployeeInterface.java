a878bac3df01d40943ce2ce6a67437efc76dfda3
Christopher Lo <christopherlo383@gmail.com>
2022-10-23 03:14:08 -0700

    added getters and setters for Employee

--- EmployeeInterface.java
+++ EmployeeInterface.java
@@ -14,4 +14,34 @@ public interface EmployeeInterface{
     /**Records time of clocking out for break */
     public void breakOut();
 
+    /**Get Employee's ID */
+    public void getID();
+
+    /**Set Employee's ID */
+    public void setID();
+
+    /**Get Employee's Name*/
+    public void getName();
+
+    /**Set Employee's Name*/
+    public void setName();
+
+    /**Get Employee's phone number*/
+    public void getPhoneNum();
+
+    /**Set Employee's phone number*/
+    public void setPhoneNum();
+
+    /**Get Employee's address*/
+    public void getAddress();
+
+    /**Set Employee's address*/
+    public void setAddress();
+
+    /**Get Employee's supervisor*/
+    public void getSupervisor();
+
+    /**Set Employee's supervisor*/
+    public void setSupervisor();
+
 }
