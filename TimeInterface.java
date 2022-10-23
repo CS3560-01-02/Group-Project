1f9541364ff20a789d88e2c7a871d6148e4f3385
alonzojp <alonzojp@gmail.com>
2022-10-23 03:08:12 -0700

    TimeInterface Update

--- TimeInterface.java
+++ TimeInterface.java
@@ -1,3 +1,27 @@
 public interface TimeInterface {
+
+    //* Saves the current time as shiftIn  */
     public void setShiftIn();
+
+    //* Gets the time saved in shiftIn  */
+    public void getShiftIn();
+
+    //* Saves the current time as shiftOut  */
+    public void setShiftOut();
+
+    //* Gets the time saved in shiftOut  */
+    public void getShiftOut();
+
+    //* Saves the current time as breakIn  */
+    public void setBreakIn();
+
+    //* Gets the time saved in breakIn  */
+    public void getBreakIn();
+
+    //* Saves the current time as breakOut  */
+    public void setBreakOut();
+
+    //* Gets the time saved in breakOut  */
+    public void getBreakOut();
+
 }
