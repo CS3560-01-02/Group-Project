6520103a1ecca6b5a3f615c1d70e73b5f201b7f5
chillwafflez <ju5t1n.nguy3n284@gmail.com>
2022-10-23 18:28:23 -0700

    modified job position class

--- JobPositionInterface.java
+++ JobPositionInterface.java
@@ -1,41 +1,67 @@
 import java.sql.Date;
 
-public interface JobPositionInterface {
+public class JobPositionInterface {
+
+    //Attributes
+    int jobID;
+    String jobTitle;
+    int rate;
+    Date employmentDate;
 
     /* Returns the ID of the job position*/
-    public int getJobID();
+    public int getJobID() {
+
+        return jobID;
+    }
 
     /**
      * Sets ID of the job position
      * @param ID The new ID.
      */
-    public void setJobID(int ID);
+    public void setJobID(int ID) {
+
+    }
 
     /* Returns the title of the job position*/
-    public String getTitle();
+    public String getTitle() {
+
+        return jobTitle;
+    }
 
     /**
      * Sets the title of the job position
      * @param title The new job title.
      */
-    public void setTitle(String title);
+    public void setTitle(String title){
+
+    }
 
     /* Returns the wage rate the job position*/
-    public int getRate();
+    public int getRate() {
+
+        return rate;
+    }
 
     /**
      * Sets the wage rate of the job position
      * @param rate The new wage rate
      */
-    public void setRate(int rate);
+    public void setRate(int rate) {
+
+    }
 
     /* Returns the employment date*/
-    public Date getEmploymentDate();
+    public Date getEmploymentDate() {
+
+        return employmentDate;
+    }
 
     /**
      * Sets the employment date of the job position
      * @param date The employment date.
      */
-    public void setEmploymentDate(Date date);
+    public void setEmploymentDate(Date date) {
+
+    }
     
 }
