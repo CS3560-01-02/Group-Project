457058b40b6240de0afe8d07382b4b7b8fad1839
chillwafflez <ju5t1n.nguy3n284@gmail.com>
2022-10-23 18:31:47 -0700

    modified request class

--- RequestInterface.java
+++ RequestInterface.java
@@ -1,52 +1,84 @@
 import java.sql.Date;
 import java.sql.Time;
 
-public interface RequestInterface {
+public class RequestInterface {
+
+    //Attributes
+    int requestID;
+    Date requestDate;
+    Time requestTime;
+    String description;
+    String status;
 
     /* Returns the ID of the request*/
-    public int getID();
+    public int getID() {
+
+        return requestID;
+    }
     
      /**
      * Sets the ID of the request
      * @param ID the new ID of request.
      */
-    public void setID(int ID);
+    public void setID(int ID) {
+
+    }
 
     /* Returns the date the request was made*/
-    public Date getDate();
+    public Date getDate() {
+
+        return requestDate;
+    }
     
     /**
      * Sets the date of the request
      * @param date the new date of request.
      */
-    public void setDate(Date date);
+    public void setDate(Date date) {
+
+    }
 
     /* Returns the time the request was made*/
-    public Time getTime();
+    public Time getTime() {
+
+        return requestTime;
+    }
     
      /**
      * Sets the time of the request
      * @param time the new time of request.
      */
-    public void setTime(Time time);
+    public void setTime(Time time) {
+
+    }
 
     /* Returns the description of the request*/
-    public String getDescription();
+    public String getDescription() {
+
+        return description;
+    }
     
     /**
      * Sets the description of the request
      * @param description the new description of request.
      */
-    public void setDescription(String description);
+    public void setDescription(String description) {
+
+    }
 
     /* Returns the status of the request*/
-    public String getStatus();
+    public String getStatus() {
+
+        return status;
+    }
     
     /**
      * Sets the status of the request
      * @param status the new status of request.
      */
-    public void setStatus(String status);
+    public void setStatus(String status) {
+        
+    }
 
     
 }
