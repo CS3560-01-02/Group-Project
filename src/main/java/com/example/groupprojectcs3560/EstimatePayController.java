c2022032c4d8575f876da492d7a8892e479b0ec8
chillwafflez <ju5t1n.nguy3n284@gmail.com>
2022-11-16 18:34:14 -0800

    added estimatePay window or scene. added estimatePay controller

---
+++ src/main/java/com/example/groupprojectcs3560/EstimatePayController.java
@@ -0,0 +1,50 @@
+package com.example.groupprojectcs3560;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class EstimatePayController {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void switchToLoginWindow(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("loginWindow.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToTimeWindow(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("timeWindow.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToTimesheetWindow(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("timesheetWindow.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToRequestWindow(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("requestWindow.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+}
