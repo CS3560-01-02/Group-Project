925df1bd4e3a8e54e084807f6cfc1d534401d940
alonzojp <alonzojp@gmail.com>
2022-11-22 18:57:00 -0800

    Fixed createTimeTest to automatically work

--- src/main/java/com/example/groupprojectcs3560/TestFiles/createTimeTest.java
+++ src/main/java/com/example/groupprojectcs3560/TestFiles/createTimeTest.java
@@ -18,6 +18,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class createTimeTest {
@@ -32,45 +33,60 @@ public class createTimeTest {
 
         Connection conn = SQLConnection.databaseConnect();
 
-        Date currentDate = null;
-        String Time_ID = "1111";
-        String Emp_ID = "1";
-        String workedDate = "2022-11-23";
-        String ShiftIn = "22:33:22";
-        String ShiftOut = "82:33:22";
-        String MealIn = "43:33:22";
-        String MealOut = "20:33:22";
+        int empID = 001;
+        Date date = new Date();
+        SimpleDateFormat currentDate = new SimpleDateFormat("YYYY-MM-dd");
+        String todayDate = currentDate.format(date);
+        String shiftIn = new SimpleDateFormat("HH:mm:ss").format(Calendar.getInstance().getTime());
+        String shiftOut = new SimpleDateFormat("HH:mm:ss").format(Calendar.getInstance().getTime());
+        String mealIn = new SimpleDateFormat("HH:mm:ss").format(Calendar.getInstance().getTime());
+        String mealOut = new SimpleDateFormat("HH:mm:ss").format(Calendar.getInstance().getTime());
 
         Statement stat = conn.createStatement();
-        String sql = "INSERT INTO TimeWorked (time_id, emp_id, workedDate, shiftIn, shiftOut, mealIn, mealOut)" + "VALUES(?, ?, ?, ?, ?, ?, ?)";
+        String sql = "INSERT INTO TimeWorked (emp_id, workedDate, shiftIn, shiftOut, mealIn, mealOut)" + "VALUES(?, ?, ?, ?, ?, ?)";
         PreparedStatement preparedStatement = conn.prepareStatement(sql);
 
-        if (clockInButtonTEST.getText().equals("Clock Out")) {
-            currentDate = new Date();
-            SimpleDateFormat timeFormat = new SimpleDateFormat("hh:mm:ss");
-
-            displayClockInTime.setText("Clocked out at " + timeFormat.format(currentDate));
-            clockInButtonTEST.setText("Clock In");
-        } else {
-
-            currentDate = new Date();
-            SimpleDateFormat timeFormat = new SimpleDateFormat("hh:mm:ss");
-
-            displayClockInTime.setText("Clocked in at " + timeFormat.format(currentDate));
-            clockInButtonTEST.setText("Clock Out");
-        }
-
-        preparedStatement.setInt(1, Integer.parseInt(Time_ID));
-        preparedStatement.setInt(2, Integer.parseInt(Emp_ID));
-        preparedStatement.setString(3, workedDate);
-        preparedStatement.setString(4, ShiftIn);
-        preparedStatement.setString(5, ShiftOut);
-        preparedStatement.setString(6, MealIn);
-        preparedStatement.setString(7, MealOut);
-
+        preparedStatement.setInt(1, empID);
+        preparedStatement.setString(2, todayDate);
+        preparedStatement.setString(3, shiftIn);
+        preparedStatement.setString(4, shiftOut);
+        preparedStatement.setString(5, mealIn);
+        preparedStatement.setString(6, mealOut);
 
         preparedStatement.executeUpdate();
+
         SQLConnection.databaseDisconnect(conn);
+
+//        Statement stat = conn.createStatement();
+//        String sql = "INSERT INTO TimeWorked (time_id, emp_id, workedDate, shiftIn, shiftOut, mealIn, mealOut)" + "VALUES(?, ?, ?, ?, ?, ?, ?)";
+//        PreparedStatement preparedStatement = conn.prepareStatement(sql);
+//
+//        if (clockInButtonTEST.getText().equals("Clock Out")) {
+//            currentDate = new Date();
+//            SimpleDateFormat timeFormat = new SimpleDateFormat("hh:mm:ss");
+//
+//            displayClockInTime.setText("Clocked out at " + timeFormat.format(currentDate));
+//            clockInButtonTEST.setText("Clock In");
+//        } else {
+//
+//            currentDate = new Date();
+//            SimpleDateFormat timeFormat = new SimpleDateFormat("hh:mm:ss");
+//
+//            displayClockInTime.setText("Clocked in at " + timeFormat.format(currentDate));
+//            clockInButtonTEST.setText("Clock Out");
+//        }
+//
+//        preparedStatement.setInt(1, Integer.parseInt(Time_ID));
+//        preparedStatement.setInt(2, Integer.parseInt(Emp_ID));
+//        preparedStatement.setString(3, workedDate);
+//        preparedStatement.setString(4, ShiftIn);
+//        preparedStatement.setString(5, ShiftOut);
+//        preparedStatement.setString(6, MealIn);
+//        preparedStatement.setString(7, MealOut);
+//
+//
+//        preparedStatement.executeUpdate();
+//        SQLConnection.databaseDisconnect(conn);
 
     }
 
