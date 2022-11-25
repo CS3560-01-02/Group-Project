762db9ac2bf5ead1d8ea72d62b23e441c3661b74
Justin Nguyen <ju5t1n.nguy3n284@gmail.com>
2022-11-24 19:54:18 -0800

    modified view time sheet to show last 5 records for an employee

--- src/main/java/com/example/groupprojectcs3560/MainApplication.java
+++ src/main/java/com/example/groupprojectcs3560/MainApplication.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("loginWindow.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("viewTimeSheetTEST.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
--- src/main/java/com/example/groupprojectcs3560/TestFiles/creatingTimesheetTest.java
+++ src/main/java/com/example/groupprojectcs3560/TestFiles/creatingTimesheetTest.java
@@ -7,7 +7,6 @@ import com.example.groupprojectcs3560.SQLConnection;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -42,16 +41,17 @@ public class creatingTimesheetTest   {
     @FXML
     public TableColumn<TimeWorked, Integer> totalHoursColumn;
 
+    private ObservableList<TimeWorkedTESTObject> TimeRecords = createTimeWorkedObservableList();
 
 
-
-    //Test objects to put into table view || what we would do is create these objects using data from the 5 records we get and then yuhhh
-    TimeWorkedTESTObject test1 = new TimeWorkedTESTObject(1, "10/21/22", "06:32:54", ":32:54", "12:32:54", "12:32:54", 40);
-    TimeWorkedTESTObject test2 = new TimeWorkedTESTObject(1, "11/06/22", "08:21:03", "yuh", "hai", "bruh", 69);
-    TimeWorkedTESTObject test3 = new TimeWorkedTESTObject(1, "hurb", "man", "1medw", "12:32:54", "12ede54", 90);
-    TimeWorkedTESTObject test4 = new TimeWorkedTESTObject(1, "yay", "12:beuh", "cock", "w", "12:32:54", 31);
-    TimeWorkedTESTObject test5 = getLast5Records();
-
+    /*
+       //Test objects to put into table view || what we would do is create these objects using data from the 5 records we get and then yuhhh
+       TimeWorkedTESTObject test1 = new TimeWorkedTESTObject(1, "10/21/22", "06:32:54", ":32:54", "12:32:54", "12:32:54", 40);
+       TimeWorkedTESTObject test2 = new TimeWorkedTESTObject(1, "11/06/22", "08:21:03", "yuh", "hai", "bruh", 69);
+       TimeWorkedTESTObject test3 = new TimeWorkedTESTObject(1, "hurb", "man", "1medw", "12:32:54", "12ede54", 90);
+       TimeWorkedTESTObject test4 = new TimeWorkedTESTObject(1, "yay", "12:beuh", "cock", "w", "12:32:54", 31);
+       TimeWorkedTESTObject test5 = createTimeWorkedObject();
+    */
     public creatingTimesheetTest() throws SQLException {
 
     }
@@ -69,11 +69,15 @@ public class creatingTimesheetTest   {
         timeSheetTable.setItems(TimeRecords);
     }
 
-    public TimeWorkedTESTObject getLast5Records() throws SQLException {
+
+    public ObservableList<TimeWorkedTESTObject> createTimeWorkedObservableList() throws SQLException {
+
+        ObservableList<TimeWorkedTESTObject> temp = FXCollections.observableArrayList();
+        TimeWorkedTESTObject currentRow = null;
 
         //Variables to insert into constructor for the TimeWorked objects that will be put into TableView
         int timeRecordIDAttribute = 0;
-        int employeeId = 1;
+        int employeeId = 1;     //change later to get static variable employee id of who is logged in
         String workedDateAttribute = null;
         String clockInAttribute = null;
         String clockOutAttribute = null;
@@ -83,35 +87,79 @@ public class creatingTimesheetTest   {
 
         Connection conn = SQLConnection.databaseConnect();
         ResultSet rs = null;
+        String sql = null;
 
-        //hardcoded for now
-        String sql = "select * from timeworked where emp_id = '1' order by time_id desc limit 1;";
+        /*in this for loop, you get the 5 latest records of the TimeWorked table, create TimeWorkedObjects for each,
+         * and then you add those objects into an Observable List*/
+        for (int i = 0; i < 5; i++) {
+           /*this if statement basically allows us to get the latest record in first iteration of for loop,
+           and then we use i to get the rest of the latest records before the first*/
+            if (i == 0) {
+                sql = "select * from timeworked where emp_id = '1' order by time_id desc limit 1;";
+            } else {
+                sql = "select * from timeworked where emp_id = '1' order by time_id desc limit 1," + i + ";";
+            }
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            rs = preparedStatement.executeQuery();
 
-        PreparedStatement preparedStatement = conn.prepareStatement(sql);
-        rs  = preparedStatement.executeQuery();
-
-        while(rs.next()){
-            timeRecordIDAttribute = rs.getInt("time_id");
-            workedDateAttribute = rs.getString("workedDate");
-            clockInAttribute = rs.getString("shiftIn");
-            clockOutAttribute = rs.getString("shiftOut");
-            // mealInAttribute = rs.getString("mealIn");
-            // mealOutAttribute = rs.getString("mealIn");
+            while (rs.next()) {
+                timeRecordIDAttribute = rs.getInt("time_id");
+                workedDateAttribute = rs.getString("workedDate");
+                clockInAttribute = rs.getString("shiftIn");
+                clockOutAttribute = rs.getString("shiftOut");
+                // mealInAttribute = rs.getString("mealIn");
+                // mealOutAttribute = rs.getString("mealIn");
+            }
+            currentRow = new TimeWorkedTESTObject(timeRecordIDAttribute, workedDateAttribute, clockInAttribute, clockOutAttribute, mealInAttribute, mealOutAttribute, totalHoursWorkedAttribute);
+            temp.add(currentRow);
         }
-
-        TimeWorkedTESTObject timeSheetRow = new TimeWorkedTESTObject(timeRecordIDAttribute, workedDateAttribute, clockInAttribute, clockOutAttribute, mealInAttribute, mealOutAttribute, totalHoursWorkedAttribute);
         SQLConnection.databaseDisconnect(conn);
-        return timeSheetRow;
+        return temp;
     }
 
-    private ObservableList<TimeWorkedTESTObject> TimeRecords = FXCollections.observableArrayList(
-            test1,
-            test2,
-            test3,
-            test4,
-            test5
-    );
+//OG code just in case this doesn't work
+/*
+   public TimeWorkedTESTObject createTimeWorkedObject() throws SQLException {
 
+       //Variables to insert into constructor for the TimeWorked objects that will be put into TableView
+       int timeRecordIDAttribute = 0;
+       int employeeId = 1;
+       String workedDateAttribute = null;
+       String clockInAttribute = null;
+       String clockOutAttribute = null;
+       String mealInAttribute = "texas";
+       String mealOutAttribute = "lappland";
+       int totalHoursWorkedAttribute = 20;
+
+       Connection conn = SQLConnection.databaseConnect();
+       ResultSet rs = null;
+
+       //hardcoded for now
+       String sql = "select * from timeworked where emp_id = '1' order by time_id desc limit 1;";
+
+       PreparedStatement preparedStatement = conn.prepareStatement(sql);
+       rs  = preparedStatement.executeQuery();
+
+       while(rs.next()){
+           timeRecordIDAttribute = rs.getInt("time_id");
+           workedDateAttribute = rs.getString("workedDate");
+           clockInAttribute = rs.getString("shiftIn");
+           clockOutAttribute = rs.getString("shiftOut");
+           // mealInAttribute = rs.getString("mealIn");
+           // mealOutAttribute = rs.getString("mealIn");
+       }
+
+       TimeWorkedTESTObject timeSheetRow = new TimeWorkedTESTObject(timeRecordIDAttribute, workedDateAttribute, clockInAttribute, clockOutAttribute, mealInAttribute, mealOutAttribute, totalHoursWorkedAttribute);
+       SQLConnection.databaseDisconnect(conn);
+       return timeSheetRow;
+   }
+
+   private ObservableList<TimeWorkedTESTObject> TimeRecords = FXCollections.observableArrayList(
+           test1,
+           test2,
+           test3,
+           test4,
+           test5
+   ); */
 
 }
-
