cc71a707e1c2f537d356a4abe04b8c2378302b6f
Justin Nguyen <ju5t1n.nguy3n284@gmail.com>
2022-11-25 02:26:29 -0800

    allowed supervisor to view recent requests and approve requests, updating corresponding records yuhhhhh

--- src/main/java/com/example/groupprojectcs3560/ViewRequestController.java
+++ src/main/java/com/example/groupprojectcs3560/ViewRequestController.java
@@ -1,15 +1,21 @@
 package com.example.groupprojectcs3560;
 
+import com.example.groupprojectcs3560.ModelClasses.Employee;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class ViewRequestController {
 
@@ -29,6 +35,59 @@ public class ViewRequestController {
     Button SwitchToCreateEmployeeScene;
     @FXML
     Button normalViewButton;
+    @FXML
+    Button viewLatestRequest;
+    @FXML
+    TextField fetchedRequest;
+    @FXML
+    Button approveRequest;
+    int requestID = -1;
+
+    public void initialize() {
+
+        fetchedRequest.setEditable(false);
+        approveRequest.setDisable(true);
+    }
+
+    public void getLatestRequest(ActionEvent event) throws IOException, SQLException {
+
+        String requestDescription = "";
+        Connection conn = SQLConnection.databaseConnect();
+        int currentID = Employee.empID;
+        ResultSet rs = null;
+
+        String sql = "SELECT * FROM request JOIN employee WHERE request.emp_id = employee.emp_id AND sup_id = " + currentID + " AND approved = 0 ORDER BY reqDate ASC limit 1;";
+
+        PreparedStatement preparedStatement = conn.prepareStatement(sql);
+
+        rs = preparedStatement.executeQuery();
+
+        while (rs.next()) {
+            requestDescription = rs.getString("description");
+            requestID = rs.getInt("req_id");
+        }
+
+        if (requestDescription.equals("")) {
+            fetchedRequest.setText("No requests made");
+        } else {
+            fetchedRequest.setText(requestDescription);
+            approveRequest.setDisable(false);
+        }
+
+    } //end getLatestRequest
+
+    public void approveRequest(ActionEvent event) throws SQLException {
+
+        Connection conn = SQLConnection.databaseConnect();
+
+        String setApprovedToTrue = "UPDATE request SET approved = true WHERE req_id = " + requestID + ";";
+        PreparedStatement preparedStatementTRUE = conn.prepareStatement(setApprovedToTrue);
+        preparedStatementTRUE.executeUpdate();
+
+        approveRequest.setDisable(true);
+        fetchedRequest.setText("");
+    }
+
 
     public void switchToTimeWindow(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("timeWindow.fxml"));
