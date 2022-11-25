5f7682f0c70389f5fd91d7e5d6a9ed10238786c2
Justin Nguyen <ju5t1n.nguy3n284@gmail.com>
2022-11-25 00:01:00 -0800

    added supervisor screens

---
+++ src/main/java/com/example/groupprojectcs3560/ViewRequestController.java
@@ -0,0 +1,64 @@
+package com.example.groupprojectcs3560;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ViewRequestController {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    @FXML
+    Button SwitchToSearchEmployeeScene;
+    @FXML
+    Button SwitchToViewRequestScene;
+    @FXML
+    Button LogoutButtonForSupervisor;
+    @FXML
+    Button SwitchToEditTimesheetScene;
+    @FXML
+    Button SwitchToCreateEmployeeScene;
+
+
+    public void switchSearchEmployeeScene(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("searchEmployeeWindow.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToEditTimeSheetScene(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("editTimeSheet.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToCreateEmployeeScene(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("createEmployeeWindow.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToLoginWindow(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("loginWindow.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+}
